a3e9d068d748ea0df7784e57a27b7285acd24f9a
Jesse PJ <jesse@parisjourdan.com>
2024-05-30 01:07:15 +1000

    refactor query_polygons()

--- src/pg.c
+++ src/pg.c
@@ -9,81 +9,84 @@ Polygon_array *query_polygons(PGconn *db, Memory_Context *context, char *query)
     PGresult *result = PQexecParams(db, query, 0, NULL, NULL, NULL, NULL, 1);
     if (PQresultStatus(result) != PGRES_TUPLES_OK)  return QueryError("Query failed: %s", PQerrorMessage(db));
 
-    int num_tuples = PQntuples(result);
-    if (num_tuples == 0)  return QueryError("A query for polygons returned no results.");
+    int num_rows = PQntuples(result);
+    if (num_rows == 0)  return QueryError("A query for polygons returned no results.");
 
     int column = PQfnumber(result, "polygon");
     if (column < 0)  return QueryError("We couldn't find a \"polygon\" column in the results.");
 
     Polygon_array *polygons = NewArray(polygons, context);
 
-    for (int row = 0; row < num_tuples; row++) {
+    for (int row = 0; row < num_rows; row++) {
+        int num_bytes = PQgetlength(result, row, column);
+        if (!num_bytes)  continue;
+
         char *data = PQgetvalue(result, row, column);
         char *d = data;
 
-        u8 byte_order = *d;
-        d += sizeof(u8);
-        assert(byte_order == WKB_LITTLE_ENDIAN);
+        s64 num_geometries = 1;
 
-        u32 wkb_type;  memcpy(&wkb_type, d, sizeof(u32));
-        d += sizeof(u32);
+        while (num_geometries > 0) {
+            num_geometries -= 1;
 
-        // We want this function to work for both WKB_POLYGON and WKB_MULTIPOLYGON data.
-        u32 num_polygons;
-        if (wkb_type == WKB_POLYGON) {
-            num_polygons = 1;
-        } else if (wkb_type == WKB_MULTIPOLYGON) {
-            memcpy(&num_polygons, d, sizeof(u32));
-            d += sizeof(u32);
-        } else {
-            return QueryError("We expected WKB_POLYGON (3) or WKB_MULTIPOLYGON (6). Instead we got %d.", wkb_type);
-        }
+            u8 byte_order = *d;
+            d += sizeof(u8);
+            assert(byte_order == WKB_LITTLE_ENDIAN);
 
-        while (num_polygons--) {
-            Polygon polygon = {.context = context};
-
-            if (wkb_type == WKB_MULTIPOLYGON) {
-                // It's a multipolygon. We need to parse the byte order and data type again for each polygon.
-                // This is ugly, particularly the reuse of the byte_order and wkb_type variables. @Cleanup.
-                u8 byte_order = *d;
-                d += sizeof(u8);
-                assert(byte_order == WKB_LITTLE_ENDIAN);
-
-                u32 wkb_type;  memcpy(&wkb_type, d, sizeof(u32));
-                d += sizeof(u32);
-                assert(wkb_type == WKB_POLYGON);
-            }
-
-            u32 num_rings;  memcpy(&num_rings, d, sizeof(u32));
+            u32 wkb_type;  memcpy(&wkb_type, d, sizeof(u32));
             d += sizeof(u32);
 
-            while (num_rings--) {
-                Path ring = {.context = context};
+            switch (wkb_type) {
+                case WKB_GEOMETRYCOLLECTION:
+                case WKB_MULTIPOLYGON:;
+                    // We treat GeometryCollections and MultiPolygons the same: we just add to the
+                    // number of extra geometries to expect in the results. This means we'll accept
+                    // technically invalid data such as GeometryCollections inside MultiPolygons.
+                    // Since we're just consuming this data from Postgres, I don't think we need to
+                    // be strict about making sure it's well-formed.
+                    u32 num_extra;  memcpy(&num_extra, d, sizeof(u32));
+                    d += sizeof(u32);
 
-                u32 num_points;  memcpy(&num_points, d, sizeof(u32));
-                d += sizeof(u32);
+                    num_geometries += num_extra;
+                    continue;
+                case WKB_POLYGON:;
+                    Polygon polygon = {.context = context};
 
-                while (num_points--) {
-                    double x;  memcpy(&x, d, sizeof(double));
-                    d += sizeof(double);
+                    u32 num_rings;  memcpy(&num_rings, d, sizeof(u32));
+                    d += sizeof(u32);
 
-                    double y;  memcpy(&y, d, sizeof(double));
-                    d += sizeof(double);
+                    for (u32 ring_index = 0; ring_index < num_rings; ring_index += 1) {
+                        Path ring = {.context = context};
 
-                    // Cast doubles to floats.
-                    *Add(&ring) = (Vector2){
-                        (float)x,
-                        -(float)y // Flip the y-axis.
-                    };
-                }
-                *Add(&polygon) = ring;
+                        u32 num_points;  memcpy(&num_points, d, sizeof(u32));
+                        d += sizeof(u32);
+
+                        for (u32 point_index = 0; point_index < num_points; point_index += 1) {
+                            double x;  memcpy(&x, d, sizeof(double));
+                            d += sizeof(double);
+
+                            double y;  memcpy(&y, d, sizeof(double));
+                            d += sizeof(double);
+
+                            // Cast doubles to floats.
+                            *Add(&ring) = (Vector2){
+                                (float)x,
+                                -(float)y // Flip the y-axis.
+                            };
+                        }
+
+                        *Add(&polygon) = ring;
+                    }
+
+                    *Add(polygons) = polygon;
+                    break;
+                default:
+                    Error("Unexpected wkb_type: %d.", wkb_type);
             }
-            *Add(polygons) = polygon;
         }
 
         // Check that the number of bytes parsed by this function is equal to the Postgres reported size.
-        s64 num_bytes_parsed = d - data;
-        assert(num_bytes_parsed == PQgetlength(result, row, column));
+        assert(d - data == num_bytes);
     }
 
     PQclear(result);
@@ -92,7 +95,7 @@ Polygon_array *query_polygons(PGconn *db, Memory_Context *context, char *query)
 }
 
 Path_array *query_paths(PGconn *db, Memory_Context *context, char *query)
-// @Copypasta from query_polygons().
+// Old @Copypasta from query_polygons().
 {
     PGresult *result = PQexecParams(db, query, 0, NULL, NULL, NULL, NULL, 1);
     if (PQresultStatus(result) != PGRES_TUPLES_OK)  return QueryError("Query failed: %s", PQerrorMessage(db));
