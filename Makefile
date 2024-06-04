014ba7fe1859c32479fb90c75e4b615a801ba21f
Jesse PJ <jesse@parisjourdan.com>
2024-06-04 18:04:42 +1000

    stop voronoi from crossing electorate bounds

--- Makefile
+++ Makefile
@@ -8,12 +8,12 @@ cflags += -Wall -Werror
 cflags += -Wno-unused
 cflags += -std=c99 # The lack of -pedantic allows anonymous unions in structs.
 cflags += -g3
-#cflags += -O2
+cflags += -O2
 cflags += -MMD -MP
 cflags += -MT bin/$*.o
 cflags += -o $@
-cflags += -DDEBUG
-#cflags += -DNDEBUG
+#cflags += -DDEBUG
+cflags += -DNDEBUG
 cflags += -I/usr/include/postgresql
 cflags += $(fsan)
 
--- queries/booths.sql
+++ queries/booths.sql
@@ -49,35 +49,66 @@ projected as (
   where lat is not null
     and lon is not null
 ),
+district as (
+  -- @Todo: This is where we should be clipping the districts by the coastline.
+  select 
+    f.*,
+    st_makevalid(
+      st_simplifypreservetopology( -- @Bug: This doesn't actually seem to preserve the topology. Maybe we need to aggregate the districts first for this to work?
+        st_force2d(f.geom),
+        100.0
+      )
+    ) as bounds,
+    st_collect(p.point) as points
+  from federal_boundaries_2022 f
+  join projected p
+    on st_contains(f.geom, p.point)
+  group by f.gid
+),
 voronoi as (
   select
-    (st_dump(
-      st_voronoipolygons(
-        st_collect(
-          point
-        ),
-        10.0
-      )
-    )).geom as polygon
-  from projected
+    (st_dump(st_voronoipolygons(
+      district.points,
+      100.0
+    ))).geom as polygon,
+  district.bounds
+  from district
 ),
-all_aust as (
-  select st_makevalid(
-    st_simplify(
-      st_union(geom),
-      1000
-    )
-  ) as geom from aust_coast
+coast as (
+  select
+    st_makevalid(
+      st_simplifypreservetopology(
+        st_union(
+          geom
+        ),
+        100.0
+      )
+    ) as polygon
+  from aust_coast
   where feat_code = 'mainland'
+  or (feat_code = 'island' and area > 0.01)
+),
+clipped as (
+  select
+    st_makevalid(st_intersection(
+      st_makevalid(st_collectionextract(st_intersection(
+        voronoi.polygon,
+        voronoi.bounds,
+        10.0
+      ), 3)),
+      coast.polygon,
+      10.0
+    )) as geom
+  from voronoi
+  join coast on true
 )
 select
-  --st_astext(
   st_asbinary(
-    st_intersection(
-      voronoi.polygon,
-      all_aust.geom
+    st_makevalid(
+      st_collectionextract(
+        clipped.geom,
+        3 -- return polygons only
+      )
     )
   ) as polygon
-from voronoi
-inner join all_aust on true
-;
+from clipped;
