66434d0961c40a076396d311ac8a588923794e54
Jesse PJ <jesse@parisjourdan.com>
2024-06-10 22:16:21 +1000

    grab.h

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "map.h"
 #include "strings.h"
 
+#include "grab.h" // @Leak
+
 int main()
 {
     Memory_Context *ctx = new_context(NULL);
@@ -14,20 +16,21 @@ int main()
 
     Vertex_array *verts = NewArray(verts, ctx);
 
-    float tolerance = 50.0; // "Pixel width in metres".
+    float metres_per_pixel = 300.0;
 
     // Draw electorate boundaries as polygons.
     {
-        char *query =
-        "   select st_asbinary(st_buildarea(topo)) as polygon               "
-        "   from (                                                          "
-        "       select st_simplify(topo, $1::float) as topo                 "
-        "       from electorates_22                                         "
-        "     ) t                                                           ";
+        char *query = Grab(/*
+            select st_asbinary(st_buildarea(topo)) as polygon
+            from (
+                select st_simplify(topo, $1::float) as topo
+                from electorates_22
+              ) t
+        */);
 
         string_array *params = NewArray(params, ctx);
 
-        *Add(params) = get_string(ctx, "%f", tolerance)->data;
+        *Add(params) = get_string(ctx, "%f", metres_per_pixel)->data;
 
         Polygon_array *polygons = query_polygons(db, query, params, ctx);
 
@@ -40,30 +43,33 @@ int main()
         }
     }
 
-//    // Draw electorate boundaries as lines.
-//    {
-//        char *query =
-//        "  select st_asbinary(t.geom) as path                      "
-//        "  from (                                                  "
-//        "      select st_simplify(geom, $1::float) as geom         "
-//        "      from electorates_22_topo.edge_data                  "
-//        "    ) t                                                   ";
-//
-//        string_array *params = NewArray(params, ctx);
-//
-//        *Add(params) = get_string(ctx, "%f", tolerance)->data;
-//
-//        Path_array *paths = query_paths(db, query, params, ctx);
-//
-//        for (s64 i = 0; i < paths->count; i++) {
-//            Vector4 colour = {0.9, 0.9, 0.9, 1.0};
-//            float   width  = tolerance/8;
-//
-//            Vertex_array *path_verts = draw_path(&paths->data[i], width, colour, ctx);
-//
-//            add_verts(verts, path_verts);
-//        }
-//    }
+    // Draw electorate boundaries as lines.
+    {
+        char *query = Grab(/*
+            select st_asbinary(t.geom) as path
+            from (
+                select st_simplify(geom, $1::float) as geom
+                from electorates_22_topo.edge_data
+              ) t
+        */);
+
+        string_array *params = NewArray(params, ctx);
+
+        *Add(params) = get_string(ctx, "%f", metres_per_pixel)->data;
+
+        Path_array *paths = query_paths(db, query, params, ctx);
+
+        for (s64 i = 0; i < paths->count; i++) {
+            Vector4 colour = {0.9, 0.9, 0.9, 1.0};
+
+            float line_width_px = 5;
+            float line_width    = line_width_px*metres_per_pixel;
+
+            Vertex_array *path_verts = draw_path(&paths->data[i], line_width, colour, ctx);
+
+            add_verts(verts, path_verts);
+        }
+    }
 
     write_array_to_file(verts, "/home/jpj/src/webgl/bin/vertices");
 
