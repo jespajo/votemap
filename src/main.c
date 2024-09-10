ec2cf095d7705ab57a3e9591c891416eab9fd3e5
Jesse PJ <jesse@parisjourdan.com>
2024-09-10 23:52:13 +1000

    restore old main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,158 @@
-#include <stdio.h>
+#include <string.h>
+
+#include "draw.h"
+#include "http.h"
+#include "json.h"
+#include "map.h"
+#include "pg.h"
+#include "strings.h"
+
+#define DATABASE_URL "postgres://postgres:postgisclarity@osm.tal/gis"
+
+Response serve_vertices(Request *request, Memory_Context *context)
+{
+    Memory_Context *ctx = context;
+
+    PGconn *db = connect_to_database(DATABASE_URL);
+
+    bool show_voronoi = request->query && *Get(request->query, "voronoi");
+    float metres_per_pixel = 4000.0; //|Todo: Get this from the query string.
+
+    Vertex_array *verts = NewArray(verts, ctx);
+
+    if (show_voronoi) {
+        // Draw the Voronoi map polygons.
+        char *query =
+          " select st_asbinary(st_buildarea(topo)) as polygon  "
+          " from (                                             "
+          "     select st_simplify(topo, $1::float) as topo    "
+          "     from booths_22                                 "
+          "     where topo is not null                         "
+          "   ) t                                              ";
+
+        string_array *params = NewArray(params, ctx);
+        *Add(params) = get_string(ctx, "%f", metres_per_pixel)->data;
+
+        Polygon_array *polygons = query_polygons(db, query, params, ctx);
+
+        for (s64 i = 0; i < polygons->count; i++) {
+            Vector4 colour = {0.3*frand(), 0.8*frand(), 0.4*frand(), 1.0};
+
+            draw_polygon(&polygons->data[i], colour, verts);
+        }
+    } else {
+        // Draw electorate boundaries as polygons.
+        char *query =
+          " select st_asbinary(st_buildarea(topo)) as polygon  "
+          " from (                                             "
+          "     select st_simplify(topo, $1::float) as topo    "
+          "     from electorates_22                            "
+          "   ) t                                              ";
+
+        string_array *params = NewArray(params, ctx);
+
+        *Add(params) = get_string(ctx, "%f", metres_per_pixel)->data;
+
+        Polygon_array *polygons = query_polygons(db, query, params, ctx);
+
+        for (s64 i = 0; i < polygons->count; i++) {
+            float shade = frand();
+            Vector4 colour = {0.9*shade, 0.4*shade, 0.8*shade, 1.0};
+
+            draw_polygon(&polygons->data[i], colour, verts);
+        }
+    }
+
+    // Draw electorate boundaries as lines.
+    {
+        char *query =
+          " select st_asbinary(t.geom) as path                 "
+          " from (                                             "
+          "     select st_simplify(geom, $1::float) as geom    "
+          "     from electorates_22_topo.edge_data             "
+          "   ) t                                              ";
+
+        string_array *params = NewArray(params, ctx);
+
+        *Add(params) = get_string(ctx, "%f", metres_per_pixel)->data;
+
+        Path_array *paths = query_paths(db, query, params, ctx);
+
+        for (s64 i = 0; i < paths->count; i++) {
+            Vector4 colour = {0, 0, 0, 1.0};
+
+            float line_width_px = 5;
+            float line_width    = line_width_px*metres_per_pixel;
+
+            draw_path(&paths->data[i], line_width, colour, verts);
+        }
+    }
+
+    Response response = {200, .body = verts->data, .size = verts->count*sizeof(Vertex)};
+
+    response.headers = NewDict(response.headers, ctx);
+    *Set(response.headers, "content-type") = "application/octet-stream";
+
+    return response;
+}
+
+Response serve_labels(Request *request, Memory_Context *context)
+{
+    Memory_Context *ctx = context;
+
+    PGconn *db = connect_to_database(DATABASE_URL);
+
+    char *query =
+      " select jsonb_build_object(                                                               "
+      "     'labels', jsonb_agg(                                                                 "
+      "         jsonb_build_object(                                                              "
+      "             'text', upper(name),                                                         "
+      "             'pos', jsonb_build_array(round(st_x(centroid)), round(-st_y(centroid)))      "
+      "           )                                                                              "
+      "       )                                                                                  "
+      "   )::text as json                                                                        "
+      " from (                                                                                   "
+      "     select name,                                                                         "
+      "       st_centroid(geom) as centroid                                                      "
+      "     from electorates_22                                                                  "
+      "     order by st_area(geom) desc                                                          "
+      "   ) t;                                                                                   ";
+
+    Postgres_result *result = query_database(db, query, NULL, ctx);
+
+    u8_array *json = *Get(result->data[0], "json");
+
+    Response response = {200, .body = json->data, .size = json->count};
+
+    response.headers = NewDict(response.headers, ctx);
+    *Set(response.headers, "content-type") = "application/json";
+
+    return response;
+}
 
 int main()
 {
-    puts("hello.");
+    u32  const ADDR    = 0xac1180e0; // 172.17.128.224 |Todo: Use getaddrinfo().
+    u16  const PORT    = 6008;
+    bool const VERBOSE = false;
+
+    Memory_Context *top_context = new_context(NULL);
+
+    PGconn *database = connect_to_database(DATABASE_URL);
+
+    Server *server = create_server(ADDR, PORT, VERBOSE, top_context);
+
+    *Add(&server->routes) = (Route){GET, "/bin/vertices",    &serve_vertices};
+    *Add(&server->routes) = (Route){GET, "/bin/labels.json", &serve_labels};
+    *Add(&server->routes) = (Route){GET, "/.*",              &serve_file_NEW};
+
+    start_server(server);
+
+
+
+    PQfinish(database);
+
+    free_context(top_context);
+
     return 0;
 }
