e4cdc1ce8126b81bfd3ca0915a7c60ec54094519
Jesse PJ <jesse@parisjourdan.com>
2024-09-29 13:27:13 +1000

    map.h module "improvements"
    - maps now work more like arrays. you can initialise them on the stack and they will take care of their own memory when you operate on them. previously you always had to initialise a map with NewMap or NewDict, which would return a pointer to memory allocated on the heap.
    - some better documentation.
    - removed strict aliasing violations, at the cost of passing lots more arguments to the functions. i don't want to talk about it.
    - no apparent difference in performance.

--- src/map.c
+++ src/map.c
@@ -104,25 +104,18 @@ u64 hash_string(char *string)
     return (hash) ? hash : 1;
 }
 
-typedef Map(char, char) Alias_map;
-
-void *new_map(Memory_context *context, u64 key_size, u64 val_size, bool string_mode)
+bool init_map_if_needed(void **keys, void **vals, s64 *count, s64 *limit, u64 key_size, u64 val_size, Hash_bucket **buckets, s64 *num_buckets, Memory_context *context)
+// Return true if the map needed initting.
 {
+    if (*keys)  return false;
+
+    // Initialise the map.
     s64 INITIAL_KV_LIMIT    = 8; // Limit on key/value pairs. Includes the first one which is reserved, accessed as keys[-1] and vals[-1].
     s64 INITIAL_NUM_BUCKETS = 8;
 
-    Alias_map *map = New(Alias_map, context);
-
-    map->context      = context;
-
-    map->key_size     = key_size;
-    map->val_size     = val_size;
-
-    map->string_mode  = string_mode;
-
-    map->limit        = INITIAL_KV_LIMIT;
-    map->keys         = alloc(map->limit, key_size, context); //|Speed: We should allocate the keys and vals arrays together, always.
-    map->vals         = alloc(map->limit, val_size, context);
+    *limit = INITIAL_KV_LIMIT;
+    *keys  = alloc(*limit, key_size, context); //|Speed: We should allocate the keys and vals arrays together, always. The only minor annoyance is making sure the vals are properly aligned.
+    *vals  = alloc(*limit, val_size, context); //|
 
     // Reserve the first key/value pair. keys[-1] will be used as temporary storage for a key we're operating on with Get(), Set() or Delete().
     // vals[-1] will store the default value for *Get() to return if the requested key is not present.
@@ -130,51 +123,51 @@ void *new_map(Memory_context *context, u64 key_size, u64 val_size, bool string_m
     // This means that when you use *Get() with an incorrect key, the result is a zeroed-out value of the same type as map's values.
     // We rely on this behaviour in application code. Use SetDefault() to change the default value returned on a per-map basis.
     // The default default will always be the zeroed-out value.
-    memset(map->vals, 0, val_size);
-    map->keys += key_size;
-    map->vals += val_size;
+    memset(*vals, 0, val_size);
+    *keys = (u8 *)*keys + key_size;
+    *vals = (u8 *)*vals + val_size;
 
-    map->num_buckets = INITIAL_NUM_BUCKETS;
-    map->buckets     = New(map->num_buckets, Hash_bucket, context);
+    *num_buckets = INITIAL_NUM_BUCKETS;
+    *buckets     = New(*num_buckets, Hash_bucket, context);
 
-    return map;
+    return true;
 }
 
-static void grow_map_if_needed(void *map)
+void grow_map_if_needed(void **keys, void **vals, s64 *count, s64 *limit, u64 key_size, u64 val_size, Hash_bucket **buckets, s64 *num_buckets, Memory_context *context)
 {
-    Alias_map *m = map;
+    bool is_new_map = init_map_if_needed(keys, vals, count, limit, key_size, val_size, buckets, num_buckets, context);
 
-    if (m->count >= m->limit-1) {
+    if (is_new_map)  return;
+
+    if (*count >= *limit-1) {
         // We're out of room in the key/value arrays.
-        m->keys -= m->key_size;
-        m->vals -= m->val_size;
+        *keys = (u8 *)*keys - key_size;
+        *vals = (u8 *)*vals - val_size;
+        *keys = resize(*keys, 2*(*limit), key_size, context);
+        *vals = resize(*vals, 2*(*limit), val_size, context);
+        *keys = (u8 *)*keys + key_size;
+        *vals = (u8 *)*vals + val_size;
 
-        m->keys = resize(m->keys, 2*m->limit, m->key_size, m->context);
-        m->vals = resize(m->vals, 2*m->limit, m->val_size, m->context);
-
-        m->keys += m->key_size;
-        m->vals += m->val_size;
-
-        m->limit *= 2;
+        *limit *= 2;
     }
 
-    if (m->count >= m->num_buckets/4*3) {
+    if (*count >= *num_buckets/4*3) {
         // More than 3/4 of the buckets are used.
-        Hash_bucket *new_buckets = New(2*m->num_buckets, Hash_bucket, m->context);
+        Hash_bucket *new_buckets = New(2*(*num_buckets), Hash_bucket, context);
 
-        for (s64 old_i = 0; old_i < m->num_buckets; old_i++) {
-            if (!m->buckets[old_i].hash)  continue;
+        for (s64 old_index = 0; old_index < *num_buckets; old_index++) {
+            if (!(*buckets)[old_index].hash)  continue;
 
-            s64 new_i = m->buckets[old_i].hash % (2*m->num_buckets);
-            while (new_buckets[new_i].hash) {
-                new_i -= 1;
-                if (new_i < 0)  new_i += 2*m->num_buckets;
+            s64 new_index = (*buckets)[old_index].hash % (2*(*num_buckets));
+            while (new_buckets[new_index].hash) {
+                new_index -= 1;
+                if (new_index < 0)  new_index += 2*(*num_buckets);
             }
-            new_buckets[new_i] = m->buckets[old_i];
+            new_buckets[new_index] = (*buckets)[old_index];
         }
-        dealloc(m->buckets, m->context);
-        m->buckets = new_buckets;
-        m->num_buckets *= 2;
+        dealloc(*buckets, context);
+        *buckets = new_buckets;
+        *num_buckets *= 2;
     }
 }
 
@@ -187,116 +180,106 @@ static char *copy_string(char *source, Memory_context *context)
     return copy;
 }
 
-s64 set_key(void *map)
+s64 set_key(void *keys, s64 *count, u64 key_size, Hash_bucket *buckets, s64 num_buckets, Memory_context *context, bool string_mode)
 // Assume the key to set is stored in map->keys[-1]. Add the key to the map's hash table if it
 // wasn't already there and return the key's index in the map->keys array.
 {
-    Alias_map *m = map;
+    assert(context);
+    assert(*count < num_buckets); // Assume empty buckets exist so the while loops below aren't infinite loops.
 
-    assert(m->context);
-    assert(m->count < m->num_buckets); // Assume empty buckets exist so the while loops below aren't infinite loops.
-
-    grow_map_if_needed(m);
-
-    if (m->string_mode) {
+    if (string_mode) {
         // The key is a C-style string.
-        char *key = ((char **)m->keys)[-1];
+        char *key = ((char **)keys)[-1];
         u64  hash = hash_string(key);
-        s64 bucket_index = hash % m->num_buckets;
+        s64 bucket_index = hash % num_buckets;
         while (true) {
-            if (!m->buckets[bucket_index].hash) {
+            if (!buckets[bucket_index].hash) {
                 // The bucket is empty. We'll take it.
-                s64 kv_index = m->count;
-                ((char **)m->keys)[kv_index] = copy_string(key, m->context);
-                m->buckets[bucket_index].hash  = hash;
-                m->buckets[bucket_index].index = kv_index;
-                m->count += 1;
+                s64 kv_index = *count;
+                ((char **)keys)[kv_index] = copy_string(key, context);
+                buckets[bucket_index].hash  = hash;
+                buckets[bucket_index].index = kv_index;
+                *count += 1;
                 return kv_index;
             }
-            if (m->buckets[bucket_index].hash == hash) {
+            if (buckets[bucket_index].hash == hash) {
                 // The hashes match. Make sure the keys match.
-                s64 kv_index = m->buckets[bucket_index].index;
-                if (!strcmp(key, ((char **)m->keys)[kv_index]))  return kv_index;
+                s64 kv_index = buckets[bucket_index].index;
+                if (!strcmp(key, ((char **)keys)[kv_index]))  return kv_index;
             }
             bucket_index -= 1;
-            if (bucket_index < 0)  bucket_index += m->num_buckets;
+            if (bucket_index < 0)  bucket_index += num_buckets;
         }
     } else {
         // The key is binary.
-        void *key = (char *)m->keys - m->key_size; // key = m->keys[-1]
-        u64  hash = hash_bytes(key, m->key_size);
-        s64 bucket_index = hash % m->num_buckets;
+        void *key = (u8 *)keys - key_size; // key = map->keys[-1]
+        u64  hash = hash_bytes(key, key_size);
+        s64 bucket_index = hash % num_buckets;
         while (true) {
-            if (!m->buckets[bucket_index].hash) {
+            if (!buckets[bucket_index].hash) {
                 // The bucket is empty. We'll take it.
-                s64 kv_index = m->count;
-                memcpy(m->keys + kv_index*m->key_size, key, m->key_size);
-                m->buckets[bucket_index].hash  = hash;
-                m->buckets[bucket_index].index = kv_index;
-                m->count += 1;
+                s64 kv_index = *count;
+                memcpy((u8 *)keys + kv_index*key_size, key, key_size);
+                buckets[bucket_index].hash  = hash;
+                buckets[bucket_index].index = kv_index;
+                *count += 1;
                 return kv_index;
             }
-            if (m->buckets[bucket_index].hash == hash) {
+            if (buckets[bucket_index].hash == hash) {
                 // The hashes match. Make sure the keys match.
-                s64 kv_index = m->buckets[bucket_index].index;
-                if (!memcmp(key, (char *)m->keys + kv_index*m->key_size, m->key_size))  return kv_index;
+                s64 kv_index = buckets[bucket_index].index;
+                if (!memcmp(key, (u8 *)keys + kv_index*key_size, key_size))  return kv_index;
             }
             bucket_index -= 1;
-            if (bucket_index < 0)  bucket_index += m->num_buckets;
+            if (bucket_index < 0)  bucket_index += num_buckets;
         }
     }
 }
 
-s64 get_bucket_index(void *map)
+s64 get_bucket_index(void *keys, u64 key_size, Hash_bucket *buckets, s64 num_buckets, bool string_mode)
 {
-    Alias_map *m = map;
-
-    assert(m->context);
-
-    if (m->string_mode) {
+    if (string_mode) {
         // The key is a C-style string.
-        char *key = ((char **)m->keys)[-1];
+        char *key = ((char **)keys)[-1];
         u64  hash = hash_string(key);
-        s64 bucket_index = hash % m->num_buckets;
+        s64 bucket_index = hash % num_buckets;
         while (true) {
-            if (!m->buckets[bucket_index].hash)  return -1;
+            if (!buckets[bucket_index].hash)  return -1;
 
-            if (m->buckets[bucket_index].hash == hash) {
+            if (buckets[bucket_index].hash == hash) {
                 // The hashes match. Make sure the keys match.
-                s64 kv_index = m->buckets[bucket_index].index;
-                if (!strcmp(key, ((char **)m->keys)[kv_index]))  return bucket_index;
+                s64 kv_index = buckets[bucket_index].index;
+                if (!strcmp(key, ((char **)keys)[kv_index]))  return bucket_index;
             }
             bucket_index -= 1;
-            if (bucket_index < 0)  bucket_index += m->num_buckets;
+            if (bucket_index < 0)  bucket_index += num_buckets;
         }
     } else {
         // The key is binary.
-        void *key = (char *)m->keys - m->key_size; // key = m->keys[-1]
-        u64  hash = hash_bytes(key, m->key_size);
-        s64 bucket_index = hash % m->num_buckets;
+        void *key = (u8 *)keys - key_size; // key = map->keys[-1]
+        u64  hash = hash_bytes(key, key_size);
+        s64 bucket_index = hash % num_buckets;
         while (true) {
-            if (!m->buckets[bucket_index].hash)  return -1;
+            if (!buckets[bucket_index].hash)  return -1;
 
-            if (m->buckets[bucket_index].hash == hash) {
+            if (buckets[bucket_index].hash == hash) {
                 // The hashes match. Make sure the keys match.
-                s64 kv_index = m->buckets[bucket_index].index;
-                if (!memcmp(key, (char *)m->keys + kv_index*m->key_size, m->key_size))  return bucket_index;
+                s64 kv_index = buckets[bucket_index].index;
+                if (!memcmp(key, (u8 *)keys + kv_index*key_size, key_size))  return bucket_index;
             }
             bucket_index -= 1;
-            if (bucket_index < 0)  bucket_index += m->num_buckets;
+            if (bucket_index < 0)  bucket_index += num_buckets;
         }
     }
 }
 
-bool delete_key(void *map)
+bool delete_key(void *keys, void *vals, s64 *count, u64 key_size, u64 val_size, Hash_bucket *buckets, s64 num_buckets, Memory_context *context, bool string_mode)
 // Return true if the key existed.
 {
-    s64 bucket_index = get_bucket_index(map);
+    s64 bucket_index = get_bucket_index(keys, key_size, buckets, num_buckets, string_mode);
     if (bucket_index < 0)  return false;
 
-    Alias_map *m = map;
-
-    s64 kv_index = m->buckets[bucket_index].index;
+    s64 kv_index = buckets[bucket_index].index;
 
     // Delete the bucket. This is algorithm 6.4R from Knuth volume 3. Note that the errata for the
     // second edition of this book correct a significant bug in this algorithm. Step R4 should end
@@ -304,22 +287,22 @@ bool delete_key(void *map)
     {
         s64 i = bucket_index;
         while (true) {
-            m->buckets[i] = (Hash_bucket){0};
+            buckets[i] = (Hash_bucket){0};
 
             s64 j = i;
             while (true) {
                 i -= 1;
-                if (i < 0)  i += m->num_buckets;
+                if (i < 0)  i += num_buckets;
 
-                if (!m->buckets[i].hash)  goto bucket_deleted;
+                if (!buckets[i].hash)  goto bucket_deleted;
 
-                s64 r = m->buckets[i].hash % m->num_buckets;
+                s64 r = buckets[i].hash % num_buckets;
 
                 if (i <= r && r < j)  continue;
                 if (r < j && j < i)   continue;
                 if (j < i && i <= r)  continue;
 
-                m->buckets[j] = m->buckets[i];
+                buckets[j] = buckets[i];
                 break;
             }
         }
@@ -329,35 +312,35 @@ bucket_deleted:
     // Delete the key and value.
     {
         // If it's a string-mode map, delete the copy we made of the key.
-        if (m->string_mode)  dealloc(((char **)m->keys)[kv_index], m->context);
+        if (string_mode)  dealloc(((char **)keys)[kv_index], context);
 
-        if (kv_index < m->count-1) {
+        if (kv_index < *count-1) {
             // Copy the final kv pair into the places of the pair we're deleting.
-            memcpy(m->keys+m->key_size*kv_index, m->keys+m->key_size*(m->count-1), m->key_size);
-            memcpy(m->vals+m->val_size*kv_index, m->vals+m->val_size*(m->count-1), m->val_size);
+            memcpy((u8 *)keys+key_size*kv_index, (u8 *)keys+key_size*(*count-1), key_size);
+            memcpy((u8 *)vals+val_size*kv_index, (u8 *)vals+val_size*(*count-1), val_size);
 
             // Update the hash table with the new index of the pair that we moved.
-            u64 hash = (m->string_mode)
-                ? hash_string(((char **)m->keys)[m->count-1])
-                : hash_bytes(m->keys+m->key_size*(m->count-1), m->key_size);
+            u64 hash = (string_mode)
+                ? hash_string(((char **)keys)[*count-1])
+                : hash_bytes((u8 *)keys+key_size*(*count-1), key_size);
 
-            s64 bucket_index = hash % m->num_buckets;
+            s64 bucket_index = hash % num_buckets;
             while (true) {
-                if (m->buckets[bucket_index].index == m->count-1) {
-                    m->buckets[bucket_index].index = kv_index;
+                if (buckets[bucket_index].index == *count-1) {
+                    buckets[bucket_index].index = kv_index;
                     break;
                 }
-                assert(m->buckets[bucket_index].hash);
+                assert(buckets[bucket_index].hash);
                 bucket_index -= 1;
-                if (bucket_index < 0)  bucket_index += m->num_buckets;
+                if (bucket_index < 0)  bucket_index += num_buckets;
             }
         }
         // Delete the final kv pair.
-        memset(m->keys+m->key_size*(m->count-1), 0, m->key_size);
-        memset(m->vals+m->val_size*(m->count-1), 0, m->val_size);
+        memset((u8 *)keys+key_size*(*count-1), 0, key_size);
+        memset((u8 *)vals+val_size*(*count-1), 0, val_size);
     }
 
-    m->count -= 1;
+    *count -= 1;
 
     return true;
 }
--- src/map.h
+++ src/map.h
@@ -1,5 +1,3 @@
-//|Todo: Move documentation from new_map() to this header file.
-
 #ifndef MAP_H_INCLUDED
 #define MAP_H_INCLUDED
 
@@ -9,63 +7,86 @@ struct Hash_bucket {
     u64 hash;
     s64 index;
 };
-typedef struct Hash_bucket Hash_bucket;
 
-#define Map(KEY_TYPE, VAL_TYPE)    \
-    struct {                       \
-        KEY_TYPE  *keys;           \
-        VAL_TYPE  *vals;           \
-        s64        count;          \
-        s64        limit;          \
-                                   \
-        Hash_bucket *buckets;      \
-        s64          num_buckets;  \
-                                   \
-        Memory_context *context;   \
-                                   \
-        bool string_mode;          \
-        u16  key_size;             \
-        u16  val_size;             \
-        s64  i;                    \
+//
+// The .keys and .vals members are both pointers to the *second* items in allocated arrays. The first items,
+// .keys[-1] and .vals[-1], are both used for special purposes. .keys[-1] is temporary storage for whatever key
+// we're currently operating on. .vals[-1] stores the default value that *Get() returns when the key is not found.
+// We do it this way because explicit assignment to .keys[-1] and .vals[-1] is how the macros are type-safe.
+//
+// .limit refers to the number of items in each of the key-value arrays, including the skipped first member.
+//
+// .string_mode is true for dicts, which are maps where the keys are zero-terminated strings. Dicts are different
+// from normal maps in two ways: they make an internal copy of their keys and they use a different hashing function.
+//
+// .i is used for temporary storage by two of our macros. There is a subtle difference in its two uses.
+// Get() uses it to store the bucket index. Set() uses it to store the key-value index.
+//
+#define Map(KEY_TYPE, VAL_TYPE)         \
+    struct {                            \
+        KEY_TYPE       *keys;           \
+        VAL_TYPE       *vals;           \
+        s64             count;          \
+        s64             limit;          \
+                                        \
+        Hash_bucket    *buckets;        \
+        s64             num_buckets;    \
+                                        \
+        Memory_context *context;        \
+                                        \
+        bool            string_mode;    \
+        s64             i;              \
     }
 
 #define Dict(VAL_TYPE)  Map(char *, VAL_TYPE)
 
-typedef Dict(char *)  string_dict;
-typedef Dict(s64)     s64_dict;
-typedef Dict(int)     int_dict;
+typedef struct Hash_bucket Hash_bucket;
+typedef Dict(char *)       string_dict;
+typedef Dict(int)          int_dict;
 
 u64 hash_bytes(void *p, u64 size);
 u64 hash_string(char *string);
-void *new_map(Memory_context *context, u64 key_size, u64 val_size, bool string_mode);
-s64 set_key(void *map);
-s64 get_bucket_index(void *map);
-bool delete_key(void *map);
+bool init_map_if_needed(void **keys, void **vals, s64 *count, s64 *limit, u64 key_size, u64 val_size, Hash_bucket **buckets, s64 *num_buckets, Memory_context *context);
+void grow_map_if_needed(void **keys, void **vals, s64 *count, s64 *limit, u64 key_size, u64 val_size, Hash_bucket **buckets, s64 *num_buckets, Memory_context *context);
+s64 set_key(void *keys, s64 *count, u64 key_size, Hash_bucket *buckets, s64 num_buckets, Memory_context *context, bool string_mode);
+s64 get_bucket_index(void *keys, u64 key_size, Hash_bucket *buckets, s64 num_buckets, bool string_mode);
+bool delete_key(void *keys, void *vals, s64 *count, u64 key_size, u64 val_size, Hash_bucket *buckets, s64 num_buckets, Memory_context *context, bool string_mode);
 
 #define NewMap(MAP, CONTEXT) \
-    (new_map((CONTEXT), sizeof((MAP)->keys[0]), sizeof((MAP)->vals[0]), false))
+    ((MAP) = zero_alloc(1, sizeof(*MAP), (CONTEXT)), \
+     (MAP)->context = (CONTEXT), \
+     (MAP))
 
 #define NewDict(MAP, CONTEXT) \
-    (new_map((CONTEXT), sizeof((MAP)->keys[0]), sizeof((MAP)->vals[0]), true))
+    ((MAP) = zero_alloc(1, sizeof(*MAP), (CONTEXT)), \
+     (MAP)->context = (CONTEXT), \
+     (MAP)->string_mode = true, \
+     (MAP))
 
 #define Set(MAP, KEY) \
-    ((MAP)->keys[-1] = (KEY), \
-     (MAP)->i = set_key(MAP), \
+    (grow_map_if_needed((void**)&(MAP)->keys, (void**)&(MAP)->vals, &(MAP)->count, &(MAP)->limit, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), &(MAP)->buckets, &(MAP)->num_buckets, (MAP)->context), \
+     (MAP)->keys[-1] = (KEY), \
+     (MAP)->i = set_key((MAP)->keys, &(MAP)->count, sizeof(*(MAP)->keys), (MAP)->buckets, (MAP)->num_buckets, (MAP)->context, (MAP)->string_mode), \
      &(MAP)->vals[(MAP)->i])
 
 #define Get(MAP, KEY) \
-    ((MAP)->keys[-1] = (KEY), \
-     (MAP)->i = get_bucket_index(MAP), \
+    (init_map_if_needed((void**)&(MAP)->keys, (void**)&(MAP)->vals, &(MAP)->count, &(MAP)->limit, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), &(MAP)->buckets, &(MAP)->num_buckets, (MAP)->context), \
+     (MAP)->keys[-1] = (KEY), \
+     (MAP)->i = get_bucket_index((MAP)->keys, sizeof(*(MAP)->keys), (MAP)->buckets, (MAP)->num_buckets, (MAP)->string_mode), \
      &(MAP)->vals[(MAP)->i < 0 ? -1 : (MAP)->buckets[(MAP)->i].index])
 
 #define Delete(MAP, KEY) \
-    ((MAP)->keys[-1] = (KEY), \
-     delete_key(MAP))
+    (init_map_if_needed((void**)&(MAP)->keys, (void**)&(MAP)->vals, &(MAP)->count, &(MAP)->limit, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), &(MAP)->buckets, &(MAP)->num_buckets, (MAP)->context), \
+     (MAP)->keys[-1] = (KEY), \
+     delete_key((MAP)->keys, (MAP)->vals, &(MAP)->count, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), (MAP)->buckets, (MAP)->num_buckets, (MAP)->context, (MAP)->string_mode))
 
-#define SetDefault(MAP, VALUE)  ((MAP)->vals[-1] = (VALUE))
+#define SetDefault(MAP, VALUE) \
+    (init_map_if_needed((void**)&(MAP)->keys, (void**)&(MAP)->vals, &(MAP)->count, &(MAP)->limit, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), &(MAP)->buckets, &(MAP)->num_buckets, (MAP)->context), \
+     (MAP)->vals[-1] = (VALUE))
 
 #define IsSet(MAP, KEY) \
-    ((MAP)->keys[-1] = (KEY), \
-     get_bucket_index(MAP) >= 0)
+    (init_map_if_needed((void**)&(MAP)->keys, (void**)&(MAP)->vals, &(MAP)->count, &(MAP)->limit, sizeof(*(MAP)->keys), sizeof(*(MAP)->vals), &(MAP)->buckets, &(MAP)->num_buckets, (MAP)->context), \
+     (MAP)->keys[-1] = (KEY), \
+     get_bucket_index((MAP)->keys, sizeof(*(MAP)->keys), (MAP)->buckets, (MAP)->num_buckets, (MAP)->string_mode) >= 0)
 
 #endif // MAP_H_INCLUDED
